4cc4864083f2648ae002fd0230326070598a5c41
Ritu252 <66035353+Ritu252@users.noreply.github.com>
2020-08-19 23:51:58 +0530

    Add files via upload

---
+++ interactive problems.py
@@ -0,0 +1,17 @@
+x = int(input())
+def Responses(n):
+    if n<x:
+        return ("<")
+    elif n>x:
+        return (">=")
+    else:
+        return ("!"+str(x))
+for i in range(25):
+    I = Responses(int(input())
+    if (I != "!"+str(x)):
+        print(I,flush=True)
+    else:
+        break
+        
+        
+    
